f210da26ffb2c747f48c93d6a7e03d3d73af87c0
codeMAHDI <moshiurrahmanmehedi210@gmail.com>
2026-04-24 03:00:50 +0600

    add additional comments and explanations to main.py

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ print("Division:", div)
 # This is a single-line comment
 """This is a multi-line comment
 that spans multiple lines."""
+
+
+# variables
+# Python is dynamically typed, so you don't need to declare variable types, x = 5
