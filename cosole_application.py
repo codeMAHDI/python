3d817e5f35c00042236a25414a8db7fbb1db278f
codeMAHDI <moshiurrahmanmehedi210@gmail.com>
2026-05-03 00:34:34 +0600

    remove unnecessary blank lines in greeting program

--- cosole_application.py
+++ cosole_application.py
@@ -4,3 +4,26 @@ name= input("Enter your name: ") # Taking user input for name
 # print("Hello, " + name + "! Welcome to Python programming.") # Greeting the user
 
 print(f"Hello, {name} ! Welcome to Python programming.") # Using f-string for greeting the user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
